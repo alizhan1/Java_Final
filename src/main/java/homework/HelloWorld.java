cb62a25d2537180d49a537343dac410f6e3a2dd8
alizhan1 <alizhan.akhmetkaliyev@gmail.com>
2019-11-05 15:54:14 +0100

    HSU to Harbour.Space

--- src/main/java/homework/HelloWorld.java
+++ src/main/java/homework/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello World HSU!");
+        System.out.println("Hello World Harbour.Space!");
     }
 }
 
